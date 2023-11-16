69bc07607c65740ce44b701cae05fe78bdff4d42
mabrishami00 <mabrishami78@yahoo.com>
2023-11-16 20:36:54 +0330

    feat: add items related to django model

--- filmnet/filmnet/items.py
+++ filmnet/filmnet/items.py
@@ -1,12 +1,11 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
+from scrapy_djangoitem import DjangoItem
+from main.models import Movie, Genre
 
 
-class FilmnetItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class MovieItem(DjangoItem):
+    django_model = Movie
+
+
+class GenreItem(DjangoItem):
+    django_model = Genre
