1ba425cca83572f473138a55c2d20dea20a1072a
mabrishami00 <mabrishami78@yahoo.com>
2023-11-17 17:50:01 +0330

    feat: make title unique in category

--- main/models.py
+++ main/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Category(models.Model):
     type = models.CharField(max_length=100)
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, unique=True)
 
 
 class Movie(models.Model):
