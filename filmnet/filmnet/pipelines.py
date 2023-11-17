d1ea570ecd2a51e38ad4487b1f08ee60178da0b4
mabrishami00 <mabrishami78@yahoo.com>
2023-11-17 07:28:12 +0330

    feat: separate saving movie and category into the db

--- filmnet/filmnet/pipelines.py
+++ filmnet/filmnet/pipelines.py
@@ -7,13 +7,15 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 from asgiref.sync import sync_to_async
-from filmnet.filmnet.items import MovieItem
-from main.models import Movie
+from filmnet.filmnet.items import MovieItem, CategoryItem
+from main.models import Movie, Category
 import re
 
+
 def remove_tags(text):
-    clean_text = re.sub('<[^<]+?>', '', text)
+    clean_text = re.sub("<[^<]+?>", "", text)
     return clean_text
+
 
 class FilmnetPipeline:
     async def process_item(self, item, spider):
@@ -22,17 +24,25 @@ class FilmnetPipeline:
 
     @sync_to_async
     def save_item_async(self, item):
-        item["summary"] = remove_tags(item["summary"])
-        movie = Movie(
-            title=item["title"],
-            summary=item["summary"],
-            publish_date=item["publish_date"],
-            release_year=item["release_year"],
-            rate=item["rate"],
-            duration=item["duration"],
-            link=item["link"],
-        )
-        print(movie)
-        print("$"*100)
-        movie.save()
-        return movie
+        if isinstance(item, MovieItem):
+            item["summary"] = remove_tags(item["summary"])
+            print(item)
+            print("%" * 150)
+            movie = Movie(
+                title=item["title"],
+                summary=item["summary"],
+                publish_date=item["publish_date"],
+                release_year=item["release_year"],
+                rate=item["rate"],
+                duration=item["duration"],
+                link=item["link"],
+            )
+            # movie.save(commit=False)
+            movie.save()
+            movie.categories.add(*Category.objects.filter(title__in=item["categories"]))
+            movie.save()
+            return movie
+        if isinstance(item, CategoryItem):
+            category = Category(type=item["type"], title=item["title"])
+            category.save()
+            return category
