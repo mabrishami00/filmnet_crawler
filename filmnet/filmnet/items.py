a5e492c1caad86f192f448a87cb701ada2cb60d4
mabrishami00 <mabrishami78@yahoo.com>
2023-11-17 19:55:23 +0330

    feat: add artist item

--- filmnet/filmnet/items.py
+++ filmnet/filmnet/items.py
@@ -1,17 +1,21 @@
 import scrapy
 from scrapy_djangoitem import DjangoItem
-from main.models import Movie, Category
+from main.models import Movie, Category, Artist
 
 
 class MovieItem(DjangoItem):
     django_model = Movie
     categories = scrapy.item.Field()
+    artists = scrapy.item.Field()
     type = scrapy.item.Field()
 
 
 
 class CategoryItem(DjangoItem):
     django_model = Category
+
+class ArtistItem(DjangoItem):
+    django_model = Artist
 
 class ImageItem(scrapy.Item):
     image_urls = scrapy.Field()
