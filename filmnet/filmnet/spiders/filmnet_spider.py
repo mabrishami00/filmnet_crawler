3ed68f1b8a89505b1e46356fc8e2e7dd809dbfc2
mabrishami00 <mabrishami78@yahoo.com>
2023-11-17 07:26:27 +0330

    feat: get each category

--- filmnet/filmnet/spiders/filmnet_spider.py
+++ filmnet/filmnet/spiders/filmnet_spider.py
@@ -1,8 +1,8 @@
 import scrapy
 import json
 
-from main.models import Genre
-from filmnet.filmnet.items import MovieItem, GenreItem
+from main.models import Category
+from filmnet.filmnet.items import MovieItem, CategoryItem
 
 
 class FilmnetSpiderSpider(scrapy.Spider):
@@ -17,11 +17,17 @@ class FilmnetSpiderSpider(scrapy.Spider):
         data = json.loads(response.body)
         movies = data.get("data")
         for movie in movies:
-            genres = movie.get("categories", [])
-            genre_item = GenreItem()
-            for genre in genres:
-                genre_item["name"] = genre.get("type")
-                # yield genre_item
+            category_names = []
+            categories = movie.get("categories", [])
+            for category in categories:
+                items = category.get("items")
+                for item in items:
+                    category_item = CategoryItem()
+                    category_item["type"] = category.get("type")
+                    category_item["title"] = item.get("title")
+                    category_names.append(item.get("title"))
+                    yield category_item
+
             movie_item["title"] = movie.get("title")
             movie_item["summary"] = movie.get("summary")
             movie_item["publish_date"] = movie.get("published_at")
@@ -29,9 +35,11 @@ class FilmnetSpiderSpider(scrapy.Spider):
             movie_item["rate"] = movie.get("rate")
             movie_item["duration"] = movie.get("duration")
             movie_item["link"] = movie.get("link")
+            movie_item["categories"] = category_names
 
-            genre_names = [genre.get('type') for genre in genres]
-            # movie_item.instance.genres.add(*Genre.objects.filter(name__in=genre_names))
+
+            # movie_item.instance.genres.add(*Category.objects.filter(title__in=category_names))
+
 
             yield movie_item
 
