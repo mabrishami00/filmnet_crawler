f1a98d14eb35495c0eafc90a261a30f44d9516d3
mabrishami00 <mabrishami78@yahoo.com>
2023-11-17 21:07:27 +0330

    feat: add mechanism to scrape 100 movies

--- filmnet/filmnet/spiders/filmnet_spider.py
+++ filmnet/filmnet/spiders/filmnet_spider.py
@@ -8,8 +8,10 @@ from filmnet.filmnet.items import MovieItem, CategoryItem, ImageItem, ArtistItem
 class FilmnetSpiderSpider(scrapy.Spider):
     name = "filmnet_spider"
     allowed_domains = ["filmnet.ir"]
+    NUMBER_OF_MOVIES_EACH_REQUEST = 25
+    OFFSET = 0
     start_urls = [
-        "https://filmnet.ir/api-v2/video-contents?offset=0&count=5&order=latest&query=&types=single_video&types=series&types=video_content_list"
+        f"https://filmnet.ir/api-v2/video-contents?offset={OFFSET}&count={NUMBER_OF_MOVIES_EACH_REQUEST}&order=latest&query=&types=single_video&types=series&types=video_content_list"
     ]
 
     def parse(self, response):
@@ -40,26 +42,31 @@ class FilmnetSpiderSpider(scrapy.Spider):
             movie_item["categories"] = category_names
             movie_item["type"] = movie.get("type")
 
-
             if movie.get("type") == "single_video":
                 image_urls.append(movie.get("cover_image").get("path"))
-                yield scrapy.Request(link, callback=self.parse_detail, cb_kwargs={'movie_item': movie_item})
+                yield scrapy.Request(
+                    link,
+                    callback=self.parse_detail,
+                    cb_kwargs={"movie_item": movie_item},
+                )
         image_item = ImageItem()
         image_item["image_urls"] = image_urls
         yield image_item
-
+        self.OFFSET += 25
+        if self.OFFSET <= 100:
+            next_link = f"https://filmnet.ir/api-v2/video-contents?offset={self.OFFSET}&count={self.NUMBER_OF_MOVIES_EACH_REQUEST}&order=latest&query=&types=single_video&types=series&types=video_content_list"
+            yield response.follow(next_link, self.parse)
 
     def parse_detail(self, response, movie_item):
         all_tags = response.css(".css-165by6p").extract()
         artist_names = []
         for tag in all_tags:
             selector = scrapy.Selector(text=tag)
-            if selector.css('p.css-1io4wcd.e1eum8tf0::text').get() == 'بازیگر':
-                artist_name = selector.css('p.css-1wuywbg.e1eum8tf0::text').get()
+            if selector.css("p.css-1io4wcd.e1eum8tf0::text").get() == "بازیگر":
+                artist_name = selector.css("p.css-1wuywbg.e1eum8tf0::text").get()
                 artist_names.append(artist_name)
                 artist_item = ArtistItem()
                 artist_item["name"] = artist_name
                 yield artist_item
-        movie_item['artists'] = artist_names 
+        movie_item["artists"] = artist_names
         yield movie_item
-
