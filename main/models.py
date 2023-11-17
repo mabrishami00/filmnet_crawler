cf6b23cc70aefc8def68cb497ef6fd4ddb483cb4
mabrishami00 <mabrishami78@yahoo.com>
2023-11-17 19:57:20 +0330

    feat: add artist model

--- main/models.py
+++ main/models.py
@@ -1,8 +1,19 @@
 from django.db import models
 
+
 class Category(models.Model):
     type = models.CharField(max_length=100)
     title = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Movie(models.Model):
@@ -14,3 +25,7 @@ class Movie(models.Model):
     duration = models.CharField(max_length=10)
     link = models.URLField(max_length=500, null=True)
     categories = models.ManyToManyField(Category)
+    artists = models.ManyToManyField(Artist)
+
+    def __str__(self):
+        return self.title
